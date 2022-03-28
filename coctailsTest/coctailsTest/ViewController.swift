0c5162ab3a1fcd9f974b7264e847239e0c67f640
Anatoliy C <tolik.nik97@gmail.com>
2022-03-28 17:46:41 +0300

    first commit

---
+++ coctailsTest/coctailsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coctailsTest
+//
+//  Created by Anatoliy on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
